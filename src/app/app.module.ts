0d50b294030db350043f87b9e8eab19db787e87e
Pap Gergely <pap133@gmail.com>
2019-01-30 15:22:31 +0100

    create navbar, sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
+    [BrowserAnimationsModule],
+    HttpClientModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    MatSidenavModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
