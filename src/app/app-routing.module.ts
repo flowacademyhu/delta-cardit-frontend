4296976638175f7f88da92bb500b6dccb2acda3e
Dancsa89 <dancsa89@gmail.com>
2019-02-05 09:59:07 +0100

    folders and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './componets/login/login.component';
+import { NavComponent } from './componets/nav/nav.component';
+import { CardComponent } from './componets/card/card.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}
+  { path: '', component: LoginComponent},
+  { path: 'index', component: NavComponent},
+  { path: 'flashcard', component: CardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './componets/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './componets/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './componets/card/card.component';
 import {MatCardModule} from '@angular/material/card';
 
 
