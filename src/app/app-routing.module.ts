6993dc327486f62b9bf6efff582f92ec74ec817f
Pap Gergely <pap133@gmail.com>
2019-02-08 14:39:06 +0100

    routing modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { LoginComponent } from './componets/login/login.component';
 import { NavComponent } from './componets/nav/nav.component';
 import { CardComponent } from './componets/card/card.component';
 import { AuthGuard } from './services/auth.guard';
+import { CardListComponent } from './componets/card-list/card-list.component';
+import { SubjectsComponent } from './componets/subjects/subjects.component';
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'index', component: NavComponent, canActivate: [AuthGuard]},
-  { path: 'flashcard', component: CardComponent}
+  { path: 'flashcard', component: CardComponent},
+  { path: 'learningcard', component: CardListComponent},
+  { path: 'subjects', component: SubjectsComponent}
 ];
 
 @NgModule({
