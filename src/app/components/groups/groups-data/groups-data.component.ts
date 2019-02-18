e07d20323e38544be602b066312f861cc04f2063
Dancsa89 <dancsa89@gmail.com>
2019-02-18 20:15:53 +0100

    table form listing added to users

--- src/app/components/groups/groups-data/groups-data.component.ts
+++ src/app/components/groups/groups-data/groups-data.component.ts
@@ -3,6 +3,10 @@ import { GroupsService } from 'src/app/services/groups.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { GroupModel } from 'src/app/models/group.model';
 import { DecksService } from 'src/app/services/decks.service';
+import { MatDialog } from '@angular/material';
+import { DeckModel } from 'src/app/models/deck.model';
+import { group } from '@angular/animations';
+import { FooterRowOutlet } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-groups-data',
@@ -18,7 +22,8 @@ export class GroupsDataComponent implements OnInit {
     private groupService: GroupsService,
     private route: ActivatedRoute,
     private router: Router,
-    private decksService: DecksService
+    private decksService: DecksService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
@@ -29,16 +34,21 @@ export class GroupsDataComponent implements OnInit {
     this.loadGroup();
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(GroupDataDialogComponent, {
+    });
+  }
+
   getUsersByGroup(id: number) {
-    this.groupService.usersByGroupId(id).subscribe(group => {
-      this.usersData = group;
+    this.groupService.usersByGroupId(id).subscribe(result => {
+      this.usersData = result;
       console.log(this.usersData);
     });
   }
 
   getDecksByGroup(id: number) {
-    this.groupService.decksByGroupId(id).subscribe(group => {
-      this.deckData = group;
+    this.groupService.decksByGroupId(id).subscribe(result => {
+      this.deckData = result;
       console.log(this.deckData);
     });
   }
@@ -62,4 +72,39 @@ export class GroupsDataComponent implements OnInit {
       console.log('Nooooooooooo');
     });
   }
+}
+
+@Component({
+  selector: 'app-groups-data-dialog',
+  templateUrl: 'groups-data-dialog.html',
+  styleUrls: ['groups-data-dialog.scss'],
+})
+export class GroupDataDialogComponent implements OnInit {
+
+  private decks: DeckModel[] = [];
+  private deckIds: number[];
+  private deckId: number;
+
+  constructor(private dialog: MatDialog,
+    private decksService: DecksService,
+    private route: ActivatedRoute,
+    private groupService: GroupsService,
+    private groupData: GroupsDataComponent) { }
+
+    ngOnInit() {
+      this.decksService.getAllDecks().subscribe(decks => {
+        this.decks = decks;
+      });
+    }
+
+    save() {
+      const currentGroup = this.groupData.group;
+      console.log(currentGroup);
+      this.decksService.addingDecks(currentGroup.id, this.deckIds).subscribe(result => {
+        console.log('Mink vagyunk a legjobbak tesó!');
+        this.ngOnInit();
+      }, err => {
+        console.log('Szoppantyú!');
+      });
+    }
 }
