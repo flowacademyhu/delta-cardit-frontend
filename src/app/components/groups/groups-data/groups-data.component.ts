1e89f78f9e19e7eb2b120957b0246b903727ceb7
Netti992 <n.anetta@gmail.hu>
2019-02-19 10:51:10 +0100

    forget password

--- src/app/components/groups/groups-data/groups-data.component.ts
+++ src/app/components/groups/groups-data/groups-data.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { GroupsService } from 'src/app/services/groups.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { GroupModel } from 'src/app/models/group.model';
 import { DecksService } from 'src/app/services/decks.service';
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { DeckModel } from 'src/app/models/deck.model';
+import { group } from '@angular/animations';
+import { FooterRowOutlet } from '@angular/cdk/table';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-groups-data',
@@ -10,15 +15,24 @@ import { DecksService } from 'src/app/services/decks.service';
   styleUrls: ['./groups-data.component.scss']
 })
 export class GroupsDataComponent implements OnInit {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   usersData: any = [];
   deckData: any = [];
   group: GroupModel = {} as GroupModel;
+  public dataSource;
+  public deckDataSource;
+  public displayedColumns: string[] = ['name', 'email', 'role'];
+  public deckDisplayedColumns: string[] = ['name', 'edit'];
 
   constructor(
     private groupService: GroupsService,
     private route: ActivatedRoute,
     private router: Router,
-    private decksService: DecksService
+    private decksService: DecksService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
@@ -29,16 +43,27 @@ export class GroupsDataComponent implements OnInit {
     this.loadGroup();
   }
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(GroupDataDialogComponent, {
+    });
+  }
+
   getUsersByGroup(id: number) {
-    this.groupService.usersByGroupId(id).subscribe(group => {
-      this.usersData = group;
+    this.groupService.usersByGroupId(id).subscribe(result => {
+    //  this.usersData = result;
+      this.dataSource = new MatTableDataSource<UserModel>(result);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
       console.log(this.usersData);
     });
   }
 
   getDecksByGroup(id: number) {
-    this.groupService.decksByGroupId(id).subscribe(group => {
-      this.deckData = group;
+    this.groupService.decksByGroupId(id).subscribe(result => {
+    //  this.deckData = result;
+      this.deckDataSource = new MatTableDataSource<DeckModel>(result);
+      this.deckDataSource.paginator = this.paginator;
+      this.deckDataSource.sort = this.sort;
       console.log(this.deckData);
     });
   }
@@ -62,4 +87,39 @@ export class GroupsDataComponent implements OnInit {
       console.log('Nooooooooooo');
     });
   }
+}
+
+@Component({
+  selector: 'app-groups-data-dialog',
+  templateUrl: 'groups-data-dialog.html',
+  styleUrls: ['groups-data-dialog.scss'],
+})
+export class GroupDataDialogComponent implements OnInit {
+
+  private decks: DeckModel[] = [];
+  private deckIds: number[];
+  private deckId: number;
+
+  constructor(private dialog: MatDialog,
+    private decksService: DecksService,
+    private route: ActivatedRoute,
+    private groupService: GroupsService,
+    private groupData: GroupsDataComponent) { }
+
+    ngOnInit() {
+      this.decksService.getAllDecks().subscribe(decks => {
+        this.decks = decks;
+      });
+    }
+
+    save() {
+      const currentGroup = this.groupData.group;
+      console.log(currentGroup);
+      this.decksService.addingDecks(currentGroup.id, this.deckIds).subscribe(result => {
+        console.log('Mink vagyunk a legjobbak tesó!');
+        this.ngOnInit();
+      }, err => {
+        console.log('Szoppantyú!');
+      });
+    }
 }
--- src/app/components/groups/groups.component.ts
+++ src/app/components/groups/groups.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { GroupModel } from 'src/app/models/group.model';
 import { GroupsService } from 'src/app/services/groups.service';
 import { GroupDialogComponent } from './group-dialog/group-dialog.component';
-import { MatDialog, MatSnackBar } from '@angular/material';
+import { MatDialog, MatSnackBar, MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-groups',
@@ -15,10 +15,15 @@ export class GroupsComponent implements OnInit {
 
   @Output() groupDeleted = new EventEmitter<GroupModel>();
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   private groups: GroupModel[] = [];
   private sendedGroup: GroupModel = {} as GroupModel;
   private selectedGroup: GroupModel;
   private deckIdsOfGroup: number[] = [];
+  public dataSource;
+  public displayedColumns: string[] = ['id', 'name', 'edit'];
 
   constructor(private groupService: GroupsService, private dialog: MatDialog,
     private snack: MatSnackBar) {
@@ -40,8 +45,11 @@ export class GroupsComponent implements OnInit {
 
   loadGroups() {
     this.groupService.getAllGroups().subscribe(groups => {
-      this.groups = groups;
-    });
+     // this.groups = groups;
+      this.dataSource = new MatTableDataSource<GroupModel>(groups);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      });
   }
 
   getUser(id: number) {
